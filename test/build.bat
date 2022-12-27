7e44b96dbf9a8e1310daf9304bd2bbc1f150a2c6
Leif Bloomquist <github@leifbloomquist.net>
2022-12-27 18:45:32 -0500

    Test program stub

--- test/build.bat
+++ test/build.bat
@@ -1,4 +1,5 @@
 @cls
-@echo off
+@if exist *.o del *.o
+@if exist *.prg del *.prg
 
-cl65 -Oi -t c64 ..\src\C64MultiNetLib.c -o test\c64multinetlibtest.prg
+@cl65 -Oi -t c64 test.c ..\src\C64MultiNetLib.c -o multilibtest.prg
---
+++ test/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "..\src\C64MultiNetLib.h"
+
+int main(void)
+{
+    printf("Testing the C64 multi net lib...\n");
+    return EXIT_SUCCESS;
+}
